9a90e8f6c9de12c2d07ca89822adbecdbed135e0
Noel <castillo752x@gmail.com>
2019-12-17 15:57:47 -0700

    entities stub

---
+++ Caravan/src/main/java/com/skilldistillery/caravan/repositories/TripHostRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.caravan.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.caravan.entities.TripHost;
+
+public interface TripHostRepository extends JpaRepository<TripHost, Integer>{
+
+}

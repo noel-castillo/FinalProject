da4732f2a7c5167d3ca44ddca53b5d564bd150a4
Noel <castillo752x@gmail.com>
2019-12-18 15:25:41 -0700

    adventure able to post

--- Caravan/src/main/java/com/skilldistillery/caravan/repositories/UserProfileRepository.java
+++ Caravan/src/main/java/com/skilldistillery/caravan/repositories/UserProfileRepository.java
@@ -2,8 +2,9 @@ package com.skilldistillery.caravan.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.skilldistillery.caravan.entities.User;
 import com.skilldistillery.caravan.entities.UserProfile;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
-	public UserProfile findByUser_Username(String username);
+	public UserProfile findByUser(User user);
 }

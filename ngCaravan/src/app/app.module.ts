0b30186924c8f45c5f017f4af3aef7f682b7e7e3
Jesse Trew <jesse.trew@gmail.com>
2019-12-18 13:06:32 -0700

    trip service

--- ngCaravan/src/app/app.module.ts
+++ ngCaravan/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TripService } from './services/trip.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    TripService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ngCaravan/src/app/models/trip.ts
+++ ngCaravan/src/app/models/trip.ts
@@ -1,2 +1,29 @@
 export class Trip {
+  id: number;
+  host: User;
+  vehicle: Vehicle;
+  departureAddress: Address ;
+  destinationAddress: Address ;
+  description: String;
+  seatsAvailable: number;
+  cargoCapacity: number;
+  createDate: Date;
+  enabled: boolean;
+  totalCost: number;
+  miles: number;
+
+
+  // tslint:disable-next-line: max-line-length
+  constructor(host?: User, departureAddress?: Address, destinationAddress?: Address, description: String, seatsAvailable: number, cargoCapactiy: number, createDate: Date, enabled: boolean, totalCost: number, miles: number) {
+    this.host = host;
+    this.departureAddress = departureAddress;
+    this.destinationAddress = destinationAddress;
+    this.description = description;
+    this.seatsAvailable = seatsAvailable;
+    this.cargoCapacity = cargoCapactiy;
+    this.createDate = createDate;
+    this.enabled = enabled;
+    this.totalCost = totalCost;
+    this.miles = miles;
+  }
 }

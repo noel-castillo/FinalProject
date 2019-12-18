48667cd732da843332c946105dc5b320d58a583e
Jared M <mcgowanjs@gmail.com>
2019-12-17 17:12:52 -0700

    m-z tests stubbed out

---
+++ JPACaravan/src/test/java/com/skilldistillery/caravan/entities/VehicleTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.caravan.entities;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class VehicleTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Vehicle vehicle;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CaravanPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		vehicle = em.find(Vehicle.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		vehicle = null;
+	}
+
+	@Test
+	@DisplayName("Test trip Entity Mapping")
+	void test() {
+		assertNotNull(vehicle);
+	}
+
+}

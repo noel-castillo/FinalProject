cc40624b66ac41b4bad1d983eac98f1d16c16aba
Jared M <mcgowanjs@gmail.com>
2019-12-19 10:18:46 -0700

    Categories stubbed

--- ngCaravan/src/app/services/categories.service.ts
+++ ngCaravan/src/app/services/categories.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Categories } from '../models/categories';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:8090';
+  private url = 'api/categories';
+
+  constructor(private http: HttpClient) { }
+
+  index() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+    return this.http.get<Categories[]>(this.url, httpOptions).pipe(
+    catchError((err: any) => {
+      console.error(err);
+      return throwError('AdventureService.index(): Error getting all adventures');
+    })
+    );
+  }
+
 }

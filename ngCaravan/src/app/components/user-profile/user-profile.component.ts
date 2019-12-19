378c4c7c84f336faf9ba0eb251374d776b41d09b
Jared M <mcgowanjs@gmail.com>
2019-12-19 14:06:56 -0700

    stubbed out UProfile

--- ngCaravan/src/app/components/user-profile/user-profile.component.ts
+++ ngCaravan/src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,8 @@
+import { UserProfile } from './../../models/user-profile';
+import { UserProfileService } from './../../services/user-profile.service';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  // F I E L D S
+
+  userProfiles: UserProfile[] = [];
+
+
+  // C O N S T R U C T O R
+
+  constructor(private uSvc: UserProfileService, private auth: AuthService, private currentRoute: ActivatedRoute, private router: Router) { }
+
+
+  // M E T H O D S
 
   ngOnInit() {
+    this.auth.login('shaun', 'wombat1').subscribe(
+      data => {
+        console.log('Logged in');
+        this.router.navigateByUrl('addresses');
+      },
+      err => {
+        console.error('Error logging in.');
+        console.error(err);
+      }
+    );
+
+    // grabs the array of todos from the service & adds it to this component
+    // if (!this.selected && this.currentRoute.snapshot.paramMap.get('id')) {
+    console.log(this.currentRoute.snapshot.paramMap.get('id'));
+    this.uSvc.index().subscribe(
+      data => {
+        this.userProfiles = data;
+      },
+      err => console.error('ngOnInit error in UserProfile Component')
+    );
   }
+
 
 }

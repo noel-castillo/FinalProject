358aa9d24a2802127b837bce94e91b7b2c141391
CMoreno04 <cesar.jos.em26@icloud.com>
2019-12-19 10:23:58 -0700

    creates user services and auth services angular

---
+++ ngCaravan/src/app/services/auth.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError} from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  // private baseUrl = environment.baseUrl;
+  private baseUrl = 'http://localhost:8090/';
+
+  constructor(private http: HttpClient) {}
+
+  login(username, password) {
+    // Make credentials
+    const credentials = this.generateBasicAuthCredentials(username, password);
+    // Send credentials as Authorization header (this is spring security convention for basic auth)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+
+    // create request to authenticate credentials
+    return this.http.get(this.baseUrl + 'authenticate', httpOptions).pipe(
+      tap(res => {
+        localStorage.setItem('credentials', credentials);
+        return res;
+      }),
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('AuthService.login(): Error logging in.');
+      })
+    );
+  }
+
+  register(user) {
+    // create request to register a new account
+    return this.http.post(this.baseUrl + 'register', user).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('AuthService.register(): error registering user.');
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('credentials');
+  }
+
+  checkLogin() {
+    if (localStorage.getItem('credentials')) {
+      return true;
+    }
+    return false;
+  }
+
+  generateBasicAuthCredentials(username, password) {
+    return btoa(`${username}:${password}`);
+  }
+
+  getCredentials() {
+    return localStorage.getItem('credentials');
+  }
+}

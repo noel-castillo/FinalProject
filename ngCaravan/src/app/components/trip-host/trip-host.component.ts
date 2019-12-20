23464a637aadda93c34ea9f7278743ab9a034498
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-12-20 14:58:49 -0700

    commit to pull

--- ngCaravan/src/app/components/trip-host/trip-host.component.ts
+++ ngCaravan/src/app/components/trip-host/trip-host.component.ts
@@ -1,9 +1,15 @@
+import { UserProfileService } from './../../services/user-profile.service';
+import { UserProfile } from './../../models/user-profile';
+import { TripTravelerService } from 'src/app/services/trip-traveler.service';
+import { TripService } from 'src/app/services/trip.service';
 import { TripHost } from 'src/app/models/trip-host';
 import { Component, OnInit } from '@angular/core';
 import { TripHostService } from 'src/app/services/trip-host.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { NgForm } from '@angular/forms';
+import { Trip } from 'src/app/models/trip';
+import { TripTraveler } from 'src/app/models/trip-traveler';
 
 @Component({
   selector: 'app-trip-host',
@@ -16,23 +22,40 @@ export class TripHostComponent implements OnInit {
   selectedReview: TripHost;
   new = false;
   editReview: TripHost = null;
+  currentUser: UserProfile;
+
+
 
   constructor(
     private thSvc: TripHostService,
+    private tripSvc: TripService,
+    private tripTravSvc: TripTravelerService,
     private currentRoute: ActivatedRoute,
     private router: Router,
-    private auth: AuthService
+    private auth: AuthService,
+    private userProSvc: UserProfileService
     ) { }
 
   ngOnInit() {
     this.auth.login('shaun', 'wombat1').subscribe(
       data => {
-        console.log('Logged in');
 
       },
       err => {
         console.error('Error logging in.');
         console.error(err);
+      }
+    );
+    this.userProSvc.getUserInSessionProfile().subscribe(
+      data => {
+        this.currentUser = data;
+        console.log('in user pro service');
+        console.log(this.currentUser);
+        console.log(this.currentUser.id);
+      },
+      error => {
+        console.error(error);
+        this.router.navigateByUrl('not-found');
       }
     );
     this.loadReviews();

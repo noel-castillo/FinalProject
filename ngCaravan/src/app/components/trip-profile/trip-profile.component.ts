fca3338e9108080e86076d07ae2e6dad2454f340
Noel <castillo752x@gmail.com>
2019-12-21 15:39:19 -0700

    trip profile stub html front end

--- ngCaravan/src/app/components/trip-profile/trip-profile.component.ts
+++ ngCaravan/src/app/components/trip-profile/trip-profile.component.ts
@@ -10,6 +10,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { TripService } from 'src/app/services/trip.service';
 import { VehicleService } from 'src/app/services/vehicle.service';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'app-trip-profile',
   templateUrl: './trip-profile.component.html',
@@ -37,6 +39,101 @@ export class TripProfileComponent implements OnInit {
   constructor(private auth: AuthService, private tripSvc: TripService, private vehicleSvc: VehicleService, private currentRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    // tslint:disable-next-line: only-arrow-functions
+    (function($) {
+      // tslint:disable-next-line: only-arrow-functions
+      $(document).ready(function() {
+        console.log('Hello from jQuery!');
+      });
+    })(jQuery);
+
+    // tslint:disable-next-line: only-arrow-functions
+    (function($) {
+
+      /*------------------
+          Preloader
+      --------------------*/
+      // $(window).on('load', function () {
+      //     $(".loader").fadeOut();
+      //     $("#preloder").delay(200).fadeOut("slow");
+      // });
+
+      /*------------------
+          Background Set
+      --------------------*/
+      $('.set-bg').each(function() {
+        // tslint:disable-next-line: prefer-const
+        let bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+      });
+
+      /*------------------
+      Navigation
+    --------------------*/
+      $('.mobile-menu').slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+      });
+
+      $('.slicknav_nav ul ').prepend('<li class="header-right-warp"></li>');
+      $('.header-right').clone().prependTo('.slicknav_nav > ul > .header-right-warp');
+
+      /*----------------------
+          Testimonial Slider
+      -----------------------*/
+      $('.testimonial-item').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoplay: false,
+      });
+
+      /*------------------
+          Magnific Popup
+      --------------------*/
+      $('.pop-up').magnificPopup({
+        type: 'image'
+      });
+
+      /*-------------------
+      Category Select
+    --------------------- */
+      $('.ca-search').niceSelect();
+
+      /*-------------------
+      Local Select
+    --------------------- */
+      $('.lo-search').niceSelect();
+
+      /*-------------------
+      Arrange Select
+    --------------------- */
+      $('.arrange-select select').niceSelect();
+
+      /*-------------------
+      Radio Btn
+    --------------------- */
+      $('.filter-left .category-filter .category-option .co-item label').on('click', function() {
+        $('.filter-left .category-filter .category-option .co-item label').removeClass('active');
+        $(this).addClass('active');
+      });
+
+      $('.filter-left .rating-filter .rating-option .ro-item label').on('click', function() {
+        $('.filter-left .rating-filter .rating-option .ro-item label').removeClass('active');
+        $(this).addClass('active');
+      });
+
+      $('.filter-left .distance-filter .distance-option .do-item label').on('click', function() {
+        $('.filter-left .distance-filter .distance-option .do-item label').removeClass('active');
+        $(this).addClass('active');
+      });
+
+    })(jQuery);
+
 
      // grabs the array of trips from the service & adds it to this component
   // if (!this.selected && this.currentRoute.snapshot.paramMap.get('id')) {

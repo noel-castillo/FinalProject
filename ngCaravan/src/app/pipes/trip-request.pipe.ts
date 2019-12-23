e2ef60f7b9e7d9fb663f1617d9548f9070ed9686
CMoreno04 <cesar.jos.em26@icloud.com>
2019-12-23 10:20:59 -0700

    trip traveler pipe finished

--- ngCaravan/src/app/pipes/trip-request.pipe.ts
+++ ngCaravan/src/app/pipes/trip-request.pipe.ts
@@ -1,16 +1,13 @@
 import { TripTraveler } from 'src/app/models/trip-traveler';
 import { Pipe, PipeTransform } from '@angular/core';
-import { TripTraveler } from '../models/trip-traveler';
 
 @Pipe({
   name: 'tripRequest'
 })
 export class TripRequestPipe implements PipeTransform {
-  transform(request: TripTraveler[]): TripTraveler[] {
-    tripRequest: TripTraveler[] = [];
-
-
-
-    return null;
+  transform(request: TripTraveler): TripTraveler {
+    if (request.approved === false && request.status === 'pending') {
+      return request;
+    }
   }
 }

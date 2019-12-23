8465f6bad55f280773e622ddfb642e9bc666cccb
Noel <castillo752x@gmail.com>
2019-12-23 14:16:04 -0700

    syncing with Cesar

--- Caravan/src/main/java/com/skilldistillery/caravan/services/TripTravelerServiceImpl.java
+++ Caravan/src/main/java/com/skilldistillery/caravan/services/TripTravelerServiceImpl.java
@@ -22,32 +22,33 @@ public class TripTravelerServiceImpl implements TripTravelerService {
 
 	@Autowired
 	TripTravelerRepository ttRepo;
-	
+
 	@Autowired
 	TripRepository tripRepo;
-	
+
 	@Autowired
 	UserProfileRepository userProfileRepo;
-	
+
 	@Autowired
 	UserRepository userRepo;
 
 	@Override
 	public TripTraveler create(TripTraveler tripTraveler, int tid, Principal principal) {
-		
+
 		User user = userRepo.findByUsername(principal.getName());
-		UserProfile userProfile= userProfileRepo.findByUser(user);
+		UserProfile userProfile = userProfileRepo.findByUser(user);
 		Trip trip = tripRepo.findById(tid).get();
-		
-		//Iterate through existing trip travelers with matching user and trip then only change status if exists
-		for(TripTraveler element : ttRepo.findAll()) {
-			if(element.getTrip() == trip && element.getUser() == userProfile) {
+
+		// Iterate through existing trip travelers with matching user and trip then only
+		// change status if exists
+		for (TripTraveler element : ttRepo.findAll()) {
+			if (element.getTrip() == trip && element.getUser() == userProfile) {
 				System.out.println("Match Found in creating Trip Traveler");
-//				element.setStatus(tripTraveler.getStatus());
+				element.setTravelerStatus(tripTraveler.getTravelerStatus());
 				return element;
 			}
 		}
-		
+
 		tripTraveler.setUser(userProfile);
 		tripTraveler.setTrip(trip);
 		ttRepo.saveAndFlush(tripTraveler);
@@ -65,7 +66,7 @@ public class TripTravelerServiceImpl implements TripTravelerService {
 			existing.setContributionPledge(tripTraveler.getContributionPledge());
 			existing.setContributionActual(tripTraveler.getContributionActual());
 			existing.setAttended(tripTraveler.isAttended());
-//			existing.setStatus(tripTraveler.getStatus());
+			existing.setTravelerStatus(tripTraveler.getTravelerStatus());
 //			existing.setTrip(tripTraveler.getTrip());
 //			existing.setUser(tripTraveler.getUser());
 
@@ -112,5 +113,17 @@ public class TripTravelerServiceImpl implements TripTravelerService {
 		});
 
 		return hostRequest;
+	}
+
+	@Override
+	public List<TripTraveler> getMyTrips(String username) {
+		List<TripTraveler> myTrips = new ArrayList<TripTraveler>();
+
+		for (TripTraveler element : ttRepo.findAll()) {
+			if (element.getUser().getUser().getUsername().equals(username)) {
+				myTrips.add(element);
+			}
+		}
+		return null;
 	}
 }
